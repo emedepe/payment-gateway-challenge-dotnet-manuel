02d703a41c2d1f607bde3ee343d27e82a9a15b9e
Manuel Duran Pereira <emedepe@outlook.com>
2025-05-06 12:20:01 +0100

    Modify payments controller and tests

--- src/PaymentGateway.Api/Controllers/PaymentsController.cs
+++ src/PaymentGateway.Api/Controllers/PaymentsController.cs
@@ -1,26 +1,58 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-using PaymentGateway.Api.Models.Responses;
-using PaymentGateway.Api.Services;
-
 namespace PaymentGateway.Api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 public class PaymentsController : Controller
 {
-    private readonly PaymentsRepository _paymentsRepository;
+    private readonly IPaymentsRepository _paymentsRepository;
 
-    public PaymentsController(PaymentsRepository paymentsRepository)
+    private readonly IPaymentsProcessingService _paymentProcessingService;
+    private readonly IPaymentsValidationService _paymentsValidationService;
+
+    public PaymentsController(
+        IPaymentsRepository paymentsRepository,
+        IPaymentsProcessingService paymentProcessingService,
+        IPaymentsValidationService paymentsValidationService)
     {
         _paymentsRepository = paymentsRepository;
+        _paymentProcessingService = paymentProcessingService;
+        _paymentsValidationService = paymentsValidationService;
     }
 
+    /// <summary>
+    /// Retrieves a payment by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the payment.</param>
+    /// <returns>The payment details if found; otherwise, a 404 response.</returns>
     [HttpGet("{id:guid}")]
     public async Task<ActionResult<PostPaymentResponse?>> GetPaymentAsync(Guid id)
     {
         var payment = _paymentsRepository.Get(id);
 
-        return new OkObjectResult(payment);
+        return payment == null
+            ? NotFound()
+            : new OkObjectResult(payment);
+    }
+
+    /// <summary>
+    /// Processes a payment request and returns the result of the payment operation.
+    /// </summary>
+    /// <param name="request">The payment request containing card details, amount, and other payment information.</param>
+    /// <returns>
+    /// A <see cref="PostPaymentResponse"/> object with the details of the processed payment:
+    /// - Returns a 200 OK response with the payment details if the payment is accepted or declined.
+    /// - Returns a 400 Bad Request response if the payment request is invalid and rejected.
+    /// - Returns a 500 Internal Server Error response if an error occurs during payment processing.
+    /// </returns>
+    [HttpPost]
+    public async Task<ActionResult<PostPaymentResponse>> PostPaymentAsync([FromBody] PostPaymentRequest request)
+    {
+        var sanitizedRequest = _paymentsValidationService.ValidatePayment(request);
+
+        var response = await _paymentProcessingService.ProcessPaymentAsync(sanitizedRequest);
+
+        return new OkObjectResult(response);
     }
 }
---
+++ test/PaymentGateway.Api.Tests/Controllers/PaymentsControllerTests.cs
@@ -0,0 +1,277 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+using Moq;
+
+using PaymentGateway.Api.Controllers;
+using PaymentGateway.Api.Exceptions;
+
+namespace PaymentGateway.Api.Tests.Controllers;
+
+public class PaymentsControllerTests
+{
+    private readonly Random _random = new();
+    
+    [Fact]
+    public async Task GetPaymentAsync_PaymentExists_ReturnsOkObjectResult()
+    {
+        // Arrange
+        var payment = new PostPaymentResponse
+        {
+            Id = Guid.NewGuid(),
+            ExpiryYear = _random.Next(2023, 2030),
+            ExpiryMonth = _random.Next(1, 12),
+            Amount = _random.Next(1, 10000),
+            CardNumberLastFour = _random.Next(1111, 9999),
+            Currency = "GBP"
+        };
+
+        var factory = CreateWebApplicationFactory(services =>
+        {
+            services.AddSingleton<IPaymentsRepository>(provider =>
+            {
+                var paymentsRepository = new PaymentsRepository();
+                paymentsRepository.Add(payment);
+                return paymentsRepository;
+            });
+
+            services.AddScoped(provider =>
+            { 
+                var paymentProcessingServiceMock = new Mock<IPaymentsProcessingService>();
+                return paymentProcessingServiceMock.Object;
+            });
+
+            services.AddSingleton(provider =>
+            {
+                var paymentsValidationServiceMock = new Mock<IPaymentsValidationService>();
+                return paymentsValidationServiceMock.Object;
+            });
+        });
+
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync($"/api/Payments/{payment.Id}");
+        var paymentResponse = await response.Content.ReadFromJsonAsync<PostPaymentResponse>();
+        
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(paymentResponse);
+    }
+
+    [Fact]
+    public async Task GetPaymentAsync_PaymentDoesNotExist_Returns404()
+    {
+        // Arrange
+        var webApplicationFactory = new WebApplicationFactory<PaymentsController>();
+        var client = webApplicationFactory.CreateClient();
+        
+        // Act
+        var response = await client.GetAsync($"/api/Payments/{Guid.NewGuid()}");
+        
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task PostPaymentAsync_SuccessfulPayment_ReturnsOkObjectResult()
+    {
+        // Arrange
+        var paymentRequest = Helpers.CreateValidPaymentRequest();
+
+        var paymentResponse = new PostPaymentResponse
+        {
+            Id = Guid.NewGuid(),
+            Status = PaymentStatus.Authorized,
+            CardNumberLastFour = int.Parse(paymentRequest.CardNumber[^4..]),
+            ExpiryMonth = paymentRequest.ExpiryMonth,
+            ExpiryYear = paymentRequest.ExpiryYear,
+            Currency = paymentRequest.Currency,
+            Amount = paymentRequest.Amount
+        };
+
+        var factory = CreateWebApplicationFactory(services =>
+        {
+            services.AddSingleton<IPaymentsRepository>(provider =>
+            {
+                var paymentsRepository = new PaymentsRepository();
+                return paymentsRepository;
+            });
+
+            services.AddScoped(provider =>
+            { 
+                var paymentProcessingServiceMock = new Mock<IPaymentsProcessingService>();
+                paymentProcessingServiceMock
+                    .Setup(s => s.ProcessPaymentAsync(paymentRequest))
+                    .ReturnsAsync(paymentResponse);
+                return paymentProcessingServiceMock.Object;
+            });
+
+            services.AddSingleton(provider =>
+            {
+                var paymentsValidationServiceMock = new Mock<IPaymentsValidationService>();
+                paymentsValidationServiceMock
+                    .Setup(s => s.ValidatePayment(It.IsAny<PostPaymentRequest>()))
+                    .Returns(paymentRequest);
+                return paymentsValidationServiceMock.Object;
+            });
+        });
+
+        var client = factory.CreateClient();
+        
+        // Act
+        var response = await client.PostAsJsonAsync("/api/Payments/", paymentRequest);
+
+        var paymentContent = await response.Content.ReadFromJsonAsync<PostPaymentResponse>();
+        
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(paymentContent);
+    }
+
+    [Fact]
+    public async Task PostPaymentAsync_PaymentProcessingError_ReturnsBadGateway()
+    {
+        // Arrange
+        var paymentRequest = Helpers.CreateValidPaymentRequest();
+
+        var factory = CreateWebApplicationFactory(services =>
+        {
+            services.AddSingleton<IPaymentsRepository>(provider =>
+            {
+                var paymentsRepository = new PaymentsRepository();
+                return paymentsRepository;
+            });
+
+            services.AddScoped(provider =>
+            { 
+                var paymentProcessingServiceMock = new Mock<IPaymentsProcessingService>();
+                paymentProcessingServiceMock
+                .Setup(s => s.ProcessPaymentAsync(It.IsAny<PostPaymentRequest>()))
+                    .Throws(new BankInternalException());
+
+                return paymentProcessingServiceMock.Object;
+            });
+
+            services.AddSingleton(provider =>
+            {
+                var paymentsValidationServiceMock = new Mock<IPaymentsValidationService>();
+                paymentsValidationServiceMock
+                    .Setup(s => s.ValidatePayment(paymentRequest))
+                    .Returns(paymentRequest);
+                return paymentsValidationServiceMock.Object;
+            });
+        });
+
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.PostAsJsonAsync("/api/Payments/", paymentRequest);
+        
+        // Assert
+        Assert.Equal(HttpStatusCode.BadGateway, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task PostPaymentAsync_PaymentProcessingServiceUnavailable_ReturnsServiceUnavailable()
+    {
+        // Arrange
+        var paymentRequest = Helpers.CreateValidPaymentRequest();
+
+        var factory = CreateWebApplicationFactory(services =>
+        {
+            services.AddSingleton<IPaymentsRepository>(provider =>
+            {
+                var paymentsRepository = new PaymentsRepository();
+                return paymentsRepository;
+            });
+
+            services.AddScoped(provider =>
+            { 
+                var paymentProcessingServiceMock = new Mock<IPaymentsProcessingService>();
+                paymentProcessingServiceMock
+                .Setup(s => s.ProcessPaymentAsync(It.IsAny<PostPaymentRequest>()))
+                    .Throws(new BankServiceUnavailableException());
+
+                return paymentProcessingServiceMock.Object;
+            });
+
+            services.AddSingleton(provider =>
+            {
+                var paymentsValidationServiceMock = new Mock<IPaymentsValidationService>();
+                paymentsValidationServiceMock
+                    .Setup(s => s.ValidatePayment(paymentRequest))
+                    .Returns(paymentRequest);
+                return paymentsValidationServiceMock.Object;
+            });
+        });
+
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.PostAsJsonAsync("/api/Payments/", paymentRequest);
+        
+        // Assert
+        Assert.Equal(HttpStatusCode.ServiceUnavailable, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task PostPaymentAsync_ValidationFailure_ReturnsBadRequest()
+    {
+        // Arrange
+        var factory = CreateWebApplicationFactory(services =>
+        {
+            services.AddSingleton<IPaymentsRepository>(provider =>
+            {
+                var paymentsRepository = new PaymentsRepository();
+                return paymentsRepository;
+            });
+
+            services.AddScoped(provider =>
+            { 
+                var paymentProcessingServiceMock = new Mock<IPaymentsProcessingService>();
+                return paymentProcessingServiceMock.Object;
+            });
+
+            services.AddSingleton(provider =>
+            {
+                var paymentsValidationServiceMock = new Mock<IPaymentsValidationService>();
+                paymentsValidationServiceMock
+                    .Setup(s => s.ValidatePayment(It.IsAny<PostPaymentRequest>()))
+                    .Throws(new PaymentRequestValidationException([]));
+                return paymentsValidationServiceMock.Object;
+            });
+        });
+
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.PostAsJsonAsync("/api/Payments/", new PostPaymentRequest());
+        
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+
+    private WebApplicationFactory<PaymentsController> CreateWebApplicationFactory(Action<IServiceCollection> configureServices)
+    {
+        return new WebApplicationFactory<PaymentsController>()
+            .WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    var descriptor = services.SingleOrDefault(s => 
+                        s.ServiceType == typeof(IPaymentsRepository));
+
+                    if (descriptor != null)
+                    {
+                        services.Remove(descriptor);
+                    }
+                
+                configureServices(services);
+            });
+        });
+    }
+}
