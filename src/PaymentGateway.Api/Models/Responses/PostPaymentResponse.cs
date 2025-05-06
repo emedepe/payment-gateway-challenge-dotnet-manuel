0ccd7b31e472e2860214affe7986dce3f92a98d7
Manuel Duran Pereira <emedepe@outlook.com>
2025-05-06 11:32:04 +0100

    Modify models for controller

--- src/PaymentGateway.Api/Models/Responses/PostPaymentResponse.cs
+++ src/PaymentGateway.Api/Models/Responses/PostPaymentResponse.cs
@@ -1,12 +1,27 @@
-﻿namespace PaymentGateway.Api.Models.Responses;
+﻿using System.Text.Json.Serialization;
+
+namespace PaymentGateway.Api.Models.Responses;
 
 public class PostPaymentResponse
 {
+    [JsonPropertyName("id")]
     public Guid Id { get; set; }
+
+    [JsonPropertyName("status")]
     public PaymentStatus Status { get; set; }
+
+    [JsonPropertyName("card_number_last_four")]
     public int CardNumberLastFour { get; set; }
+
+    [JsonPropertyName("expiry_month")]
     public int ExpiryMonth { get; set; }
+
+    [JsonPropertyName("expiry_year")]
     public int ExpiryYear { get; set; }
-    public string Currency { get; set; }
+
+    [JsonPropertyName("currency")]
+    public string Currency { get; set; } = default!;
+
+    [JsonPropertyName("amount")]
     public int Amount { get; set; }
 }
